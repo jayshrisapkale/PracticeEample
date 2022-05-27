418a0e7565f5b0f91c0d4ffa2b47f57d8cb08abf
jayshri Sapkale <jayshri@VERINITE1.local>
2022-05-27 18:07:54 +0530

    service descovery

--- src/test/java/com/example/demo/ServicediscoveryApplicationTests.java
+++ src/test/java/com/example/demo/ServicediscoveryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.user;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class ServicediscoveryApplicationTests {
 
 	@Test
 	void contextLoads() {
