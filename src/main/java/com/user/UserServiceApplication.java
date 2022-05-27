ca320291e373987501afa44f78ab201be99e21da
jayshri Sapkale <jayshri@VERINITE1.local>
2022-05-27 18:02:26 +0530

    updated microservices

--- src/main/java/com/user/UserServiceApplication.java
+++ src/main/java/com/user/UserServiceApplication.java
@@ -2,6 +2,8 @@ package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
